cf92b874ec62d75ae714afada3d5adb25859c399
Perique <en.ray.out@gmail.com>
2019-11-22 15:14:55 +0900

    change data->date

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     published_data = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_data = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
